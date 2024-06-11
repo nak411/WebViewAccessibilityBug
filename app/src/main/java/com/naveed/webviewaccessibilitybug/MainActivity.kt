4c9470a6b3d81b48bf7e18752de4abafdec12779
Naveed <nak411@gmail.com>
2024-06-10 21:44:22 -0400

    Optimize imports

--- app/src/main/java/com/naveed/webviewaccessibilitybug/MainActivity.kt
+++ app/src/main/java/com/naveed/webviewaccessibilitybug/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.naveed.webviewaccessibilitybug.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
